83cf67feadbeea0a007db6b0ac1526adc6d1e221
Lee Stilwell <lstilwel@usc.edu>
2025-02-14 23:38:36 -0800

    delete electron frontend

--- chronos-frontend/dist/renderer/App.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const jsx_runtime_1 = require("react/jsx-runtime");
-const App = () => {
-    return ((0, jsx_runtime_1.jsx)("div", { children: (0, jsx_runtime_1.jsx)("p", { children: "Hello Electron + React + TypeScript!" }) }));
-};
-exports.default = App;
